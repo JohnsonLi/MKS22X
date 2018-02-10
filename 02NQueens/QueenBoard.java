86dfce02bf914f095401912c13f31cc3058ccdfe
Li-Johnson <jli39@stuy.edu>
2018-02-10 00:45:12 -0500

    added more stuff and tried to solve

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -7,22 +7,32 @@ public class QueenBoard{
     }
     
     private boolean addQueen(int r, int c){
-        if (board[r][c] == 0){         
-            board[r][c] = -1;
-            for (int i = c + 1; i < board.length; i++){
-                board[r][i] += 1;
-            }
-            for (int i = r + 1; i < board.length; i++){
-                r += 1; 
-                c += 1;            
-                board[r][c] += 1;
-            }    
+        if (board[r][c] <= 0){     
+            board[r][c] = -1;  
+            updateThreatened(r, c, 1); 
             return true;
         }
         return false;
     }
 
-    
+    private boolean removeQueen(int r, int c){
+        if (board[r][c] == -1){     
+            board[r][c] = 0;
+            updateThreatened(r, c, -1);  
+            return true;
+        }
+        return false;
+    }
+
+    public void updateThreatened(int r, int c, int inc){
+        for (int i = c + 1; i < board.length; i++){
+            board[r][i] += inc;
+        }
+        for (int i = 1; r + i < board.length && c + i < board.length; i++){
+            board[r + i][c + i] += inc;
+            
+        }
+    }
 
     public String toString(){
         String strBoard = "";
@@ -30,8 +40,6 @@ public class QueenBoard{
             for(int ii = 0; ii < board.length; ii++){
                 if (board[i][ii] == -1){
                     strBoard += "Q ";
-                } else if (board[i][ii] > 0){
-                    strBoard += "x ";
                 } else {
                     strBoard += "_ ";
                 }
@@ -41,11 +49,41 @@ public class QueenBoard{
         return strBoard;
     }
 
+    public void checkException(){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j] != 0){
+                    throw new IllegalStateException();
+                }
+            }
+        }
+    }
+
+    public boolean solve(){
+        checkException();
+        return solveHelper(0);
+   }
+
+   public boolean solveHelper(int c){
+       if (c >= board.length){
+           return true; 
+       }
+       for (int i = 0; i < board.length; i++){
+           if (addQueen(i, c)){
+               if(solveHelper(c + 1)){
+                   return true;
+               }
+               removeQueen(i, c);
+           }
+       }
+       return false;
+   }
+
     public static void main(String[] args){
         QueenBoard board = new QueenBoard(8);
-        board.addQueen(0,0);
-        board.addQueen(0,1);
-        board.addQueen(4,4);
+        if (board.solve()){
+            System.out.println("hi");
+        }
         System.out.println(board);
     }
     
