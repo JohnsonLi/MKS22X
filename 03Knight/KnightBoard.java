089de0c278beb64e65f138c08c22923326e36c31
Johnson Li <jli39@stuy.edu>
2018-02-13 15:07:43 -0500

    started knight's tour

---
+++ 03Knight/KnightBoard.java
@@ -0,0 +1,63 @@
+public class KnightBoard{
+    private int[][] board;
+    private int rows;
+    private int cols;
+
+    public KnightBoard(int startingRows, int startingCols){ 
+        checkException();
+        board = new int[startingRows][startingCols];
+        rows = startingRows;
+        cols = startingCols;
+    }
+
+    public String toString(){
+        String strBoard = "";
+        for (int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if (board[i][j] == 0){
+                    strBoard += "_ ";                
+                }                
+                else if (board[i][j] < 10){
+                    strBoard += " " + board[i][j] + " ";
+                } else {
+                    strBoard += board[i][j] + " ";
+                } 
+            }
+            strBoard += "\n";
+        }
+        return strBoard;
+    }
+
+    public void checkException(){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                if (board[i][j] != 0 || startingRows < 0 || startingCols < 0){
+                    throw new IllegalStateException();
+                }
+            }
+        }
+    }
+
+    
+
+    public boolean solve(int row, int col){
+        checkException();
+        return solveH(row, col, 1);
+    }
+
+    public boolean solveH(int row, int col, int level){
+        if (level == rows * cols){
+            return true;
+        }
+        
+        
+
+    }
+
+
+    public static void main(String[] args){
+        KnightBoard board = new KnightBoard(7, 7);
+        System.out.println(board);    
+    }    
+
+}
