098c330c7646ba57c4993b897032c4e4cc0f4d5b
Johnson Li <jli39@stuy.edu>
2018-03-06 15:10:31 -0500

    made the class

---
+++ 05USACO/USACO.java
@@ -0,0 +1,16 @@
+public class USACO{
+
+	public static int bronze(String filename){
+		
+	}
+
+
+	public static int silver(String filename){
+
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
