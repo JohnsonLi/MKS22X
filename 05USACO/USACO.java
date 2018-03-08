f8e1683a2b635291075bf5e8e22946215dadab9e
Johnson Li <jli39@stuy.edu>
2018-03-08 15:11:34 -0500

    made a thing to make the array

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -4,36 +4,47 @@ import java.io.*;
 public class USACO{
 
     public static int bronze(String filename){
-
-
+        int row = 0, col = 0, e,n;
+        int[][] map = new int[row][col];
+        
         try {
-
             File f = new File(filename);
             Scanner in = new Scanner(f);
 
-            r = in.next(); c = in.next(); e = in.next(); n = in.next();
-            map = new int[r][c];
-            for (int i = 0; i < r; i++) {
-                for (int a = 0; a < c; a++) {
-                    map[i][a] = in.next();
+            row = Integer.parseInt(in.next()); 
+            col = Integer.parseInt(in.next());
+            e = Integer.parseInt(in.next());
+            n = Integer.parseInt(in.next());
+
+            // System.out.println(row + " " + col);
+            map = new int[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    map[i][j] = Integer.parseInt(in.next());
                 }
             }
-        }catch (FileNotFoundException e) {
-            System.out.println(e);
+        }catch (FileNotFoundException error) {
+            System.out.println(error);
             System.exit(1);
         }
 
+        // for(int i = 0; i < e; i++){
+
+        // }
 
 
+        System.out.println(map[0][1]);
+        return 0;
     }
 
 
-    public static int silver(String filename){
+    // public static int silver(String filename){
 
-    }
+    // }
+
 
     public static void main(String[] args) {
-        
+        USACO.bronze("makelake.in");
     }
 
 }
