06512c3c179c099acf5562701834e31e4c41c6a7
Johnson Li <jli39@stuy.edu>
2018-03-08 00:45:51 -0500

    .

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,16 +1,39 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
 
-	public static int bronze(String filename){
-		
-	}
+    public static int bronze(String filename){
 
 
-	public static int silver(String filename){
+        try {
 
-	}
+            File f = new File(filename);
+            Scanner in = new Scanner(f);
 
-	public static void main(String[] args) {
-		
-	}
+            r = in.next(); c = in.next(); e = in.next(); n = in.next();
+            map = new int[r][c];
+            for (int i = 0; i < r; i++) {
+                for (int a = 0; a < c; a++) {
+                    map[i][a] = in.next();
+                }
+            }
+        }catch (FileNotFoundException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+
+
+
+    }
+
+
+    public static int silver(String filename){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 
 }
