48c02a3c6d6713df8cf7a17ed0ec81619d4ecd48
Li-Johnson <jli39@stuy.edu>
2018-03-08 23:25:46 -0500

    finished bronze

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -4,12 +4,13 @@ import java.io.*;
 public class USACO{
 
     public static int bronze(String filename){
-        int row = 0, col = 0, e,n;
-        int[][] map = new int[row][col];
-        
+        int row = 0, col = 0, e = 0, n = 0;
+        int[][] field = new int[row][col];
+        Scanner in = null;
+
         try {
             File f = new File(filename);
-            Scanner in = new Scanner(f);
+            in = new Scanner(f);
 
             row = Integer.parseInt(in.next()); 
             col = Integer.parseInt(in.next());
@@ -17,24 +18,52 @@ public class USACO{
             n = Integer.parseInt(in.next());
 
             // System.out.println(row + " " + col);
-            map = new int[row][col];
+            field = new int[row][col];
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < col; j++) {
-                    map[i][j] = Integer.parseInt(in.next());
+                    field[i][j] = Integer.parseInt(in.next());
                 }
             }
         }catch (FileNotFoundException error) {
-            System.out.println(error);
+            System.out.println("FILE NOT FOUND");
             System.exit(1);
         }
 
-        // for(int i = 0; i < e; i++){
+        for(int i = 0; i < n; i++){
+            int r_s = Integer.parseInt(in.next()) - 1; 
+            int c_s = Integer.parseInt(in.next()) - 1; 
+            int d_s = Integer.parseInt(in.next()); 
 
-        // }
+            int tallest = 0;
+            for(int j = 0; j < 3 && j + r_s < field.length; j++){
+                for(int k = 0; k < 3 && k + c_s < field[0].length; k++){
+                    if(field[r_s + j][c_s + k] > tallest){
+                        tallest = field[r_s + j][c_s + k];
+                    }
+                }
+            }
+            int newD = tallest - d_s;
+            for(int j = 0; j < 3 && j + r_s < field.length; j++){
+                for(int k = 0; k < 3 && k + c_s < field[0].length; k++){
+                    if(field[r_s + j][c_s + k] > newD){
+                        field[r_s + j][c_s + k] = newD;
+                    }
+                }
+            }
+        }
 
+        int totalStomped = 0;
+        for(int i = 0; i < field.length; i++){
+            for(int j = 0; j  < field[0].length; j++){
+                if(field[i][j] < e){
+                    totalStomped += e - field[i][j];
+                }
+            }
+        }
 
-        System.out.println(map[0][1]);
-        return 0;
+        //System.out.println(field[0][2]);
+        in.close();
+        return totalStomped * 72 * 72;
     }
 
 
@@ -44,7 +73,6 @@ public class USACO{
 
 
     public static void main(String[] args) {
-        USACO.bronze("makelake.in");
+       System.out.println( USACO.bronze("makelake.in"));
     }
-
 }
