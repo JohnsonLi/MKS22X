002a0c5917822af0768dcc9073ce9921837a32e8
Johnson Li <jli39@stuy.edu>
2018-03-20 15:11:27 -0400

    doing it the way k said to

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,9 +1,42 @@
 import java.util.*;
 
-public class Mergesort{
+public class Merge{
 
-    public static void merge(int[] ary, int left, int right){
-        int[] temp = new int[ary.length];
+    // public static void merge(int[] ary, int left, int right){
+    //     int[] temp = new int[ary.length];
+
+    //     int mid = (left + right) / 2 + 1;
+    //     int leftStart = left, start = left;
+       
+    //     while(leftStart < (left + right) / 2 + 1 && mid <= right){
+    //         if(ary[leftStart] <= ary[mid]){
+    //             temp[start] = ary[leftStart];
+    //             leftStart++;
+    //         } else {
+    //             temp[start] = ary[mid];
+    //             mid++;   
+    //         }
+    //         start++;
+    //     }
+
+    //     while(leftStart < (left + right) / 2 + 1) {
+    //         temp[start] = ary[leftStart];
+    //         leftStart++;
+    //         start++;
+    //     }
+    //     while(mid <= right){
+    //         temp[start] = ary[mid];
+    //         mid++;
+    //         start++;
+    //     }
+
+    //     for(int i = left; i <= right; i++){
+    //         ary[i] = temp[i];
+    //     }
+
+    // }
+
+    public static void merge2(int[] ary, int[] temp, int left, int right){
 
         int mid = (left + right) / 2 + 1;
         int leftStart = left, start = left;
@@ -30,69 +63,52 @@ public class Mergesort{
             start++;
         }
 
-        for(int i = left; i <= right; i++){
-            ary[i] = temp[i];
-        }
-
     }
 
-    public static void merge2(int[] ary, int left, int right){
-        int[] temp = new int[ary.length];
+    // public static void mergesort(int[] ary){
+    //     mergesortH(ary, 0, ary.length - 1);
+    // }
 
-        int mid = (left + right) / 2 + 1;
-        int leftStart = left, start = left;
-       
-        while(leftStart < (left + right) / 2 + 1 && mid <= right){
-            if(ary[leftStart] <= ary[mid]){
-                temp[start] = ary[leftStart];
-                leftStart++;
-            } else {
-                temp[start] = ary[mid];
-                mid++;   
-            }
-            start++;
-        }
+    // public static void mergesortH(int[] ary, int start, int end){
+    //     // if(start >= end){
+    //     //     return;
+    //     // }
+    //     int mid = (start + end) / 2;
 
-        while(leftStart < (left + right) / 2 + 1) {
-            temp[start] = ary[leftStart];
-            leftStart++;
-            start++;
-        }
-        while(mid <= right){
-            temp[start] = ary[mid];
-            mid++;
-            start++;
-        }
+    //     if(end > start){
+    //         mergesortH(ary, start, mid);
+    //         mergesortH(ary, mid + 1, end);
+    //         merge(ary, start, end);
+    //     }
+    // }
 
-        for(int i = left; i <= right; i++){
-            ary[i] = temp[i];
-        }
-
+    public static void mergesort2(int[] ary){
+        mergesortH2(ary, new int[ary.length] ,0 ,ary.length - 1);
     }
 
-    public static void mergesort(int[] ary){
-        mergesortH(ary, 0, ary.length - 1);
-    }
-
-    public static void mergesortH(int[] ary, int start, int end){
+    public static void mergesortH2(int[] ary, int[] temp, int start, int end){
         // if(start >= end){
         //     return;
         // }
         int mid = (start + end) / 2;
-
+        
         if(end > start){
-            mergesortH(ary, start, mid);
-            mergesortH(ary, mid + 1, end);
-            merge(ary, start, end);
+            for(int i = start; i < end; i++){
+               ary[i] = temp[i];
+            }
+
+            mergesortH2(temp, ary, start, mid);
+            mergesortH2(temp, ary, mid + 1, end);
+            merge2(ary, temp, start, end);
         }
     }
 
     public static void main(String[] args) {
         int[] ary = //{123,21313,21331,44,5467,5877588,123,123123,5,64545,67};
-        /*{213,3123123,124,54,3456,7,58,57,46363625,2,235,55,646,67};*/
+        {213,3123123,124,54,3456,7,58,57,46363625,2,235,55,646,67};
         /*{132111,111};*/
         System.out.println(Arrays.toString(ary));
-        Mergesort.mergesort(ary);
+        Merge.mergesort2(ary);
         System.out.println(Arrays.toString(ary));
     }
 
