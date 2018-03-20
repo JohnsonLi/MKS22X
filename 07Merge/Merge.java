c35f4dd5926e4c01c5e07f2873b4f1fafa7038a9
Johnson Li <jli39@stuy.edu>
2018-03-20 14:55:28 -0400

    renamed

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -36,6 +36,40 @@ public class Mergesort{
 
     }
 
+    public static void merge2(int[] ary, int left, int right){
+        int[] temp = new int[ary.length];
+
+        int mid = (left + right) / 2 + 1;
+        int leftStart = left, start = left;
+       
+        while(leftStart < (left + right) / 2 + 1 && mid <= right){
+            if(ary[leftStart] <= ary[mid]){
+                temp[start] = ary[leftStart];
+                leftStart++;
+            } else {
+                temp[start] = ary[mid];
+                mid++;   
+            }
+            start++;
+        }
+
+        while(leftStart < (left + right) / 2 + 1) {
+            temp[start] = ary[leftStart];
+            leftStart++;
+            start++;
+        }
+        while(mid <= right){
+            temp[start] = ary[mid];
+            mid++;
+            start++;
+        }
+
+        for(int i = left; i <= right; i++){
+            ary[i] = temp[i];
+        }
+
+    }
+
     public static void mergesort(int[] ary){
         mergesortH(ary, 0, ary.length - 1);
     }
@@ -53,12 +87,13 @@ public class Mergesort{
         }
     }
 
-    // public static void main(String[] args) {
-    //     int[] ary = //{123,21313,21331,44,5467,5877588,123,123123,5,64545,67};
-    //     {213,3123123,124,54,3456,7,58,57,46363625,2,235,55,646,67};
-    //     System.out.println(Arrays.toString(ary));
-    //     Mergesort.mergesort(ary);
-    //     System.out.println(Arrays.toString(ary));
-    // }
+    public static void main(String[] args) {
+        int[] ary = //{123,21313,21331,44,5467,5877588,123,123123,5,64545,67};
+        /*{213,3123123,124,54,3456,7,58,57,46363625,2,235,55,646,67};*/
+        /*{132111,111};*/
+        System.out.println(Arrays.toString(ary));
+        Mergesort.mergesort(ary);
+        System.out.println(Arrays.toString(ary));
+    }
 
 }
