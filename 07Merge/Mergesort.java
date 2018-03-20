2b0a1c314eaf2a4e863507d2e7d513c6568cd004
Li-Johnson <jli39@stuy.edu>
2018-03-20 00:34:45 -0400

    did merge a different way and now sort works

--- 07Merge/Mergesort.java
+++ 07Merge/Mergesort.java
@@ -2,44 +2,38 @@ import java.util.*;
 
 public class Mergesort{
 
-    public static void merge(int[] ary, int left, int mid, int right){
-        int leftlength = mid - left + 1;
-        int rightlength = right - mid;
+    public static void merge(int[] ary, int left, int right){
+        int[] temp = new int[ary.length];
 
-        int[] leftAry = new int[leftlength];
-        int[] rightAry = new int[rightlength];
-
-        for(int i = 0; i < leftlength; i++){
-            leftAry[i] = ary[left + i];
-        }
-        for(int i = 0; i < rightlength; i++){
-            rightAry[i] = ary[mid + 1 + i];
-        }
-
-        int leftStart = 0, rightStart = 0, start = left;
+        int mid = (left + right) / 2 + 1;
+        int leftStart = left, start = left;
        
-        while(left < leftlength && right < rightlength ){
-            if(leftAry[leftStart] <= rightAry[start]){
-                ary[start] = leftAry[leftStart];
+        while(leftStart < (left + right) / 2 + 1 && mid <= right){
+            if(ary[leftStart] <= ary[mid]){
+                temp[start] = ary[leftStart];
                 leftStart++;
             } else {
-                ary[start] = rightAry[rightStart];
-                rightStart++;
+                temp[start] = ary[mid];
+                mid++;   
             }
             start++;
         }
 
-        while(leftStart < leftlength || rightStart < rightlength){
-            if(leftStart > rightStart){
-                ary[start] = leftAry[leftStart];
-                leftStart++;
-                start++;
-            } else {
-                ary[start] = rightAry[rightStart];
-                rightStart++;
-                start++;
-            }
+        while(leftStart < (left + right) / 2 + 1) {
+            temp[start] = ary[leftStart];
+            leftStart++;
+            start++;
         }
+        while(mid <= right){
+            temp[start] = ary[mid];
+            mid++;
+            start++;
+        }
+
+        for(int i = left; i <= right; i++){
+            ary[i] = temp[i];
+        }
+
     }
 
     public static void mergesort(int[] ary){
@@ -47,21 +41,24 @@ public class Mergesort{
     }
 
     public static void mergesortH(int[] ary, int start, int end){
-        if(start > end){
-            return;
-        }
+        // if(start >= end){
+        //     return;
+        // }
         int mid = (start + end) / 2;
 
-        mergesortH(ary, start, mid);
-        mergesortH(ary, mid + 1, end);
-        merge(ary, start, mid, end);
+        if(end > start){
+            mergesortH(ary, start, mid);
+            mergesortH(ary, mid + 1, end);
+            merge(ary, start, end);
+        }
     }
 
-    public static void main(String[] args) {
-        int[] ary = {123,21313,21331,44,5467,5877588,123,123123,5,64545,67};
-        System.out.println(Arrays.toString(ary));
-        Mergesort.mergesort(ary);
-        System.out.println(Arrays.toString(ary));
-    }
+    // public static void main(String[] args) {
+    //     int[] ary = //{123,21313,21331,44,5467,5877588,123,123123,5,64545,67};
+    //     {213,3123123,124,54,3456,7,58,57,46363625,2,235,55,646,67};
+    //     System.out.println(Arrays.toString(ary));
+    //     Mergesort.mergesort(ary);
+    //     System.out.println(Arrays.toString(ary));
+    // }
 
 }
