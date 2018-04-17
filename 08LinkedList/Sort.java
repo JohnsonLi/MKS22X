2d27775d076e54e18eceffd60c7583b63eb6b4be
Li-Johnson <jli39@stuy.edu>
2018-04-16 20:11:41 -0400

    finxed radix now not n^2

--- 08LinkedList/Sort.java
+++ 08LinkedList/Sort.java
@@ -1,47 +1,53 @@
 public class Sort{
 
-
     public static void radixSort(MyLinkedListImproved<Integer> list){
-        // jrefioewhoifuiowhefouiweh
-        // @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] cup = new MyLinkedListImproved[10];
-        // for(int i = 0; i < 10; i++){
-        //     cup[i] = new MyLinkedListImproved<Integer>(); 
-        //     // System.out.println(cup[i].toString());
-        // }
-
         if(list.size() == 0){
             return;
         }
 
-        int maxDigits = (int)(Math.log10(list.get(list.max())) + 1);
+        int maxDigitsPos = (int)(Math.log10(list.get(list.max())) + 1);
+        int maxDigitsNeg = (int)(Math.log10(Math.abs(list.get(list.min()))) + 1);
+        int numDigits = maxDigitsPos > maxDigitsNeg ? maxDigitsPos : maxDigitsNeg;
         int currentDigit = 0;
 
-        //BUCKETS HAVE TO BE EMPTY AUIEFWEFFWEFOIWEFHOIWEHF
-        while(maxDigits > 0){
+        while(numDigits > 0){
             @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] cup = new MyLinkedListImproved[10];
             for(int i = 0; i < 10; i++){
                 cup[i] = new MyLinkedListImproved<Integer>(); 
             }
 
+            @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] cupNeg = new MyLinkedListImproved[10];
+            for(int i = 0; i < 10; i++){
+                cupNeg[i] = new MyLinkedListImproved<Integer>(); 
+            }
+
             for(Integer value : list){
-                cup[(int)(Math.abs(value) / Math.pow(10, currentDigit) % 10)].add(value);
+                if(value >= 0){
+                    cup[(int)(Math.abs(value) / Math.pow(10, currentDigit) % 10)].add(value);
+                }   
+            }
+
+            for(Integer value : list){
+                if(value < 0){
+                    cupNeg[9 - ((int)(Math.abs(value) / Math.pow(10, currentDigit) % 10))].add(value);
+                }   
             }
 
             list.clear();
+            for(int i = 0; i < cupNeg.length; i++){
+                list.extend(cupNeg[i]);
+            }
             for(int i = 0; i < cup.length; i++){
                 list.extend(cup[i]);
             }
 
             currentDigit++;
-            maxDigits--;
+            numDigits--;
         }
-
-        for(Integer value : list){
-            if(value < 0){
-                list.remove(value);
-                list.add(0, value);
-            }
-        }
+    }
+    
+    public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){ 
+        radixSort(data);
     }
     
     // public static void main(String[] args) {
@@ -49,12 +55,14 @@ public class Sort{
     //     list.add(-2391);
     //     list.add(11231);
     //     list.add(-2);
-    //     list.add(2312312);
+    //     list.add(231);
     //     list.add(1213);
-    //     list.add(-23421);
+    //     list.add(-23423331);
+    //     list.add(-234233321);
+    //     list.add(-234234331);
     //     list.add(154);
     //     list.add(1890);
-    //     list.add(-1);
+    //     list.add(-1000000000);
     //     System.out.println(list);
     //     Sort.radixSort(list);
     //     System.out.println(list);
