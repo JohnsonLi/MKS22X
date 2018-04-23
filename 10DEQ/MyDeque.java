3225e7c67d4f6b62a0bb097da464fdf626dd7bd4
Li-Johnson <jli39@stuy.edu>
2018-04-22 20:22:31 -0400

    finished deque

--- 10DEQ/MyDeque.java
+++ 10DEQ/MyDeque.java
@@ -1,53 +1,120 @@
-public class MyDeque<E>{
+import java.io.ObjectInputStream.GetField;
+import java.util.*;
+
+public class MyDeque<E> {
 
     public E[] deck;
     public int front = 0, back = 0, size = 0;
 
     @SuppressWarnings("unchecked")
-    public MyDeque(){
-        deck = (E[]) new Object[10]; 
+    public MyDeque() {
+        deck = (E[]) new Object[10];
     }
 
     @SuppressWarnings("unchecked")
-    public MyDeque(int initialCapacity){
-        if(initialCapacity < 0){
+    public MyDeque(int initialCapacity) {
+        if (initialCapacity < 0) {
             throw new IllegalArgumentException();
         }
-        deck = (E[]) new Object[initialCapacity]; 
+        deck = (E[]) new Object[initialCapacity];
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
     @SuppressWarnings("unchecked")
-    public void resize(){
-        if (size() == deck.length - 1) {
+    public void resize() {
+        if (size() == deck.length) {
             E[] temp = (E[]) new Object[deck.length * 2];
             for (int i = 0; i < size(); i++) {
-                temp[0] = deck[(front + i) % deck.length];
+                temp[i] = deck[(front + i) % size()];
             }
+            // System.out.println(Arrays.toString(temp));
             front = 0;
             back = size() - 1;
             deck = temp;
         }
     }
 
-    public void addFirst(E value){
-        if(value == null){
+    public void addFirst(E value) {
+        if (value == null) {
             throw new NullPointerException();
         }
         resize();
-        deck[(front + deck.length) % deck.length] = value;
-        front--; 
+        if (front == 0 && deck[front] == null) {
+            deck[front] = value;
+        } else if (front - 1 < 0) {
+            deck[deck.length - 1] = value;
+            front = deck.length - 1;
+        } else {
+            deck[front - 1] = value;
+            front--;
+        }
         size++;
-
     }
 
-    public String toString(){
+    public E removeFirst() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+        E temp = deck[front];
+        deck[front] = null;
+        front = (front + deck.length + 1) % deck.length;
+        size--;
+        return temp;
+    }
+
+    public void addLast(E value) {
+        if (value == null) {
+            throw new NullPointerException();
+        }
+        resize();
+        if (back == 0 && deck[back] == null) {
+            deck[back] = value;
+        } else if (back + 1 > deck.length - 1) {
+            deck[0] = value;
+            back = 0;
+        } else {
+            deck[back + 1] = value;
+            back++;
+        }
+        size++;
+    }
+
+    public E removeLast() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+        E temp = deck[back];
+        if (back == 0) {
+            deck[back] = null;
+            back = deck.length - 1;
+        } else {
+            deck[back] = null;
+            back--;
+        }
+        return temp;
+    }
+
+    public E getFirst() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+        return deck[front];
+    }
+
+    public E getLast() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+        return deck[back];
+    }
+
+    public String toString() {
         String deckStr = "[";
-        for(int i = 0; i < deck.length; i++){
-            if(i == deck.length - 1){
+        for (int i = 0; i < deck.length; i++) {
+            if (i == deck.length - 1) {
                 deckStr += deck[i] + "";
             } else {
                 deckStr += deck[i] + ", ";
@@ -56,18 +123,84 @@ public class MyDeque<E>{
         return deckStr += "]";
     }
 
-
-    
-
-    public static void main(String[] args) {
-        MyDeque<Integer> n = new MyDeque<Integer>();
-        // System.out.println(n.size());
-        n.addFirst(1);
-        n.addFirst(2);
-        System.out.println(n.front);
-    }
-
-
-
+    // public static void main(String[] args) {
+    //     MyDeque<Integer> n = new MyDeque<Integer>();
+    //     // System.out.println(n.size());
+    //     // System.out.println(n.front);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(4);
+    //     // n.addFirst(999);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(4);
+    //     // n.addLast(5);
+    //     n.addLast(1);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(5);
+    //     n.addLast(10);
+    //     n.addFirst(13123912);
+    //     n.addLast(23123);
+    //     System.out.println(n.getFirst() + " first");
+    //     System.out.println(n.getLast() + " last");
+    //     // n.addFirst(100);
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     // n.addFirst(100);
+    //     // n.addLast(6);
+    //     // n.addLast(6);
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // System.out.println(n.removeFirst());
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(5);
+    //     // n.addFirst(6);
+    //     // System.out.println(n.size());
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     // n.removeFirst();
+    //     System.out.println(n.size());
+    //     System.out.println(n.front);
+    //     System.out.println(n);
+    // }
 
 }
