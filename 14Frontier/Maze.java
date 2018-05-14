d484a92be23e915fe0fbfd87b42f62ea7c719704
Li-Johnson <jli39@stuy.edu>
2018-05-13 20:41:05 -0400

    works?

--- 14Frontier/Maze.java
+++ 14Frontier/Maze.java
@@ -1,144 +1,144 @@
 import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.*;
-public class Maze{
 
-    private static final String CLEAR_SCREEN =  "\033[2J";
-    private static final String HIDE_CURSOR =  "\033[?25l";
-    private static final String SHOW_CURSOR =  "\033[?25h";
-    
-    Location start,end;
-    private char[][]maze;
+public class Maze {
 
+    private static final String CLEAR_SCREEN = "\033[2J";
+    private static final String HIDE_CURSOR = "\033[?25l";
+    private static final String SHOW_CURSOR = "\033[?25h";
 
+    Location start, end;
+    private char[][] maze;
 
     /*
-    YOU MUST COMPLETE THIS METHOD!!!
-    YOU MUST COMPLETE THIS METHOD!!!
-    YOU MUST COMPLETE THIS METHOD!!!
-    */
-    public Location[] getNeighbors(Location L){
+     * YOU MUST COMPLETE THIS METHOD!!! YOU MUST COMPLETE THIS METHOD!!! YOU MUST
+     * COMPLETE THIS METHOD!!!
+     */
+    public Location[] getNeighbors(Location L) {
         Location[] locations = new Location[4];
         int x = L.getX();
         int y = L.getY();
-        if((x + 1 >= 0) && (x + 1 < maze[0].length)  && ((maze[x + 1][y] == ' ') || (maze[x + 1][y] == 'E') || (maze[x + 1][y] == '?'))){
+        if ((x + 1 >= 0) && (x + 1 < maze[0].length)
+                && ((maze[x + 1][y] == ' ') || (maze[x + 1][y] == 'E') || (maze[x + 1][y] == '?'))) {
             locations[0] = new Location(x + 1, y, L, this);
         }
-        if((x - 1 >= 0) && (x - 1 < maze[0].length)  && ((maze[x - 1][y] == ' ') || (maze[x - 1][y] == 'E') || (maze[x - 1][y] == '?'))){
+        if ((x - 1 >= 0) && (x - 1 < maze[0].length)
+                && ((maze[x - 1][y] == ' ') || (maze[x - 1][y] == 'E') || (maze[x - 1][y] == '?'))) {
             locations[1] = new Location(x - 1, y, L, this);
         }
-        if((y + 1 >= 0) && (y + 1 < maze[0].length)  && ((maze[x][y + 1] == ' ') || (maze[x][y + 1] == 'E') || (maze[x][y + 1] == '?'))){
-            System.out.println(locations[2]);
+        if ((y + 1 >= 0) && (y + 1 < maze[0].length)
+                && ((maze[x][y + 1] == ' ') || (maze[x][y + 1] == 'E') || (maze[x][y + 1] == '?'))) {
             locations[2] = new Location(x, y + 1, L, this);
         }
-        if((y - 1 >= 0) && (y - 1 < maze[0].length) && ((maze[x][y - 1] == ' ') || (maze[x][y - 1] == 'E') || (maze[x][y - 1] == '?'))){
+        if ((y - 1 >= 0) && (y - 1 < maze[0].length)
+                && ((maze[x][y - 1] == ' ') || (maze[x][y - 1] == 'E') || (maze[x][y - 1] == '?'))) {
             locations[3] = new Location(x, y - 1, L, this);
         }
         return locations;
     }
 
-    public Location getStart(){
+    public Location getStart() {
         return start;
     }
 
-    public Location getEnd(){
+    public Location getEnd() {
         return end;
     }
 
-
-    private static String go(int x,int y){
+    private static String go(int x, int y) {
         return ("\033[" + x + ";" + y + "H");
     }
 
-    private static String color(int foreground,int background){
+    private static String color(int foreground, int background) {
         return ("\033[0;" + foreground + ";" + background + "m");
     }
 
-    public void clearTerminal(){
-        System.out.println(CLEAR_SCREEN+"\033[1;1H");
+    public void clearTerminal() {
+        System.out.println(CLEAR_SCREEN + "\033[1;1H");
     }
 
-    public Maze(String filename){
+    public Maze(String filename) {
         ArrayList<char[]> lines = new ArrayList<char[]>();
-        int startr=-1, startc=-1;
-        int endr=-1,endc=-1;
-        try{
+        int startr = -1, startc = -1;
+        int endr = -1, endc = -1;
+        try {
             Scanner in = new Scanner(new File(filename));
-            while(in.hasNext()){
-            lines.add(in.nextLine().toCharArray());
+            while (in.hasNext()) {
+                lines.add(in.nextLine().toCharArray());
             }
-        }catch(FileNotFoundException e){
-            System.out.println("File not found: "+filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
             System.exit(1);
         }
         maze = new char[lines.size()][];
-        for(int i = 0; i < maze.length; i++){
-            maze[i]=lines.get(i);
+        for (int i = 0; i < maze.length; i++) {
+            maze[i] = lines.get(i);
         }
-        for(int r=0; r<maze.length;r++){
-            for(int c=0; c<maze[r].length;c++){
-            if(maze[r][c]=='S'){
-                if(startr == -1){
-                startr=r;
-                startc=c;
-                }else{
-                System.out.println("Multiple 'S' found!");
-                System.exit(0);
+        for (int r = 0; r < maze.length; r++) {
+            for (int c = 0; c < maze[r].length; c++) {
+                if (maze[r][c] == 'S') {
+                    if (startr == -1) {
+                        startr = r;
+                        startc = c;
+                    } else {
+                        System.out.println("Multiple 'S' found!");
+                        System.exit(0);
+                    }
                 }
-            }
 
-            if(maze[r][c]=='E'){
-                //erase E
-                //maze[r][c]=' ';
-                if(endr == -1){
-                endr=r;
-                endc=c;
-                }else{
-                System.out.println("Multiple 'E' found!");
-                System.exit(0);
+                if (maze[r][c] == 'E') {
+                    // erase E
+                    // maze[r][c]=' ';
+                    if (endr == -1) {
+                        endr = r;
+                        endc = c;
+                    } else {
+                        System.out.println("Multiple 'E' found!");
+                        System.exit(0);
+                    }
                 }
             }
-            }
         }
-        if(startr == -1 || endr == -1){
+        if (startr == -1 || endr == -1) {
             System.out.println("Missing 'S' or 'E' from maze.");
             System.exit(0);
 
         }
 
         /*
-        THIS IS AN IMPORTANT PART BECAUSE YOU WILL NEED TO CHANGE IT LATER!
-        The start/end Locations may need more information later when we add
-        other kinds of frontiers!
-        */
-        end = new Location(endr,endc,null,this);
-        start = new Location(startr,startc,null,this);
-        }
-
-        public String toStringColor(){
-        return toStringColor(50);
-        }
-
-        public String toStringColor(int delay){
-        try{
-            Thread.sleep(delay);
-        }catch(InterruptedException e){
-
-        }
-        return HIDE_CURSOR+CLEAR_SCREEN+go(1,1)+colorize(toString())+SHOW_CURSOR;
+         * THIS IS AN IMPORTANT PART BECAUSE YOU WILL NEED TO CHANGE IT LATER! The
+         * start/end Locations may need more information later when we add other kinds
+         * of frontiers!
+         */
+        end = new Location(endr, endc, null, this);
+        start = new Location(startr, startc, null, this);
     }
 
-    public String toString(){
+    public String toStringColor() {
+        return toStringColor(50);
+    }
+
+    public String toStringColor(int delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+
+        }
+        return HIDE_CURSOR + CLEAR_SCREEN + go(1, 1) + colorize(toString()) + SHOW_CURSOR;
+    }
+
+    public String toString() {
         int maxr = maze.length;
         int maxc = maze[0].length;
         String ans = "";
-        for(int i = 0; i < maxr * maxc; i++){
-            int row = i/maxc;
-            int col = i%maxc;
+        for (int i = 0; i < maxr * maxc; i++) {
+            int row = i / maxc;
+            int col = i % maxc;
 
-            char c =  maze[row][col];
-            ans+=c;
-            if( col == maxc-1 ){
+            char c = maze[row][col];
+            ans += c;
+            if (col == maxc - 1) {
                 ans += "\n";
             }
 
@@ -146,40 +146,36 @@ public class Maze{
         return ans + "\n";
     }
 
-    public char get(int row,int col){
+    public char get(int row, int col) {
         return maze[row][col];
     }
 
-    public void set(int row,int col, char n){
+    public void set(int row, int col, char n) {
         maze[row][col] = n;
     }
 
-    public static String colorize(String s){
+    public static String colorize(String s) {
         String ans = "";
         Scanner in = new Scanner(s);
-        while(in.hasNext()){
-            String line ="";
-            for(char c : in.nextLine().toCharArray()){
-                if(c == '#'){
-                    line+= color(37,47)+c;
-                }
-                else if(c == '@'){
-                    line+= color(33,40)+c;
-                }
-                else if(c == '?'){
-                    line+= color(37,42)+c;
-                }
-                else if(c == '.'){
-                    line+= color(36,40)+c;
-                }
-                else if(c == ' '){
-                    line+= color(35,40)+c;
-                }else{
-                    line+=color(37,40)+c;
+        while (in.hasNext()) {
+            String line = "";
+            for (char c : in.nextLine().toCharArray()) {
+                if (c == '#') {
+                    line += color(37, 47) + c;
+                } else if (c == '@') {
+                    line += color(33, 40) + c;
+                } else if (c == '?') {
+                    line += color(37, 42) + c;
+                } else if (c == '.') {
+                    line += color(36, 40) + c;
+                } else if (c == ' ') {
+                    line += color(35, 40) + c;
+                } else {
+                    line += color(37, 40) + c;
                 }
 
             }
-            ans += line+color(37,40)+"\n";
+            ans += line + color(37, 40) + "\n";
         }
         return ans;
     }
--- 14Frontier/MazeSolver.java
+++ 14Frontier/MazeSolver.java
@@ -1,86 +1,79 @@
-public class MazeSolver{
+public class MazeSolver {
     private Maze maze;
     private Frontier frontier;
     private boolean animate = true;
 
-    public MazeSolver(String mazeText){
+    public MazeSolver(String mazeText) {
         maze = new Maze(mazeText);
     }
 
-    //Default to BFS
-    public boolean solve(){ return solve(0); }
+    // Default to BFS
+    public boolean solve() {
+        return solve(0);
+    }
 
-    //mode: required to allow for alternate solve modes.
-    //0: BFS
-    //1: DFS
-    public boolean solve(int mode){
-      //initialize your frontier
-        if(mode == 0){
+    // mode: required to allow for alternate solve modes.
+    // 0: BFS
+    // 1: DFS
+    public boolean solve(int mode) {
+        // initialize your frontier
+        if (mode == 0) {
             frontier = new FrontierQueue();
-        } else if(mode == 1){
+        } else if (mode == 1) {
             frontier = new FrontierStack();
         }
 
         frontier.add(maze.getStart());
-      //while there is stuff in the frontier:
-      //  get the next location
-      //  process the location to find the locations (use the maze to do this)
-      //  check if any locations are the end, if you found the end just return true!
-      //  add all the locations to the frontier
-        while(frontier.hasNext()){
-            if(animate){
-                // clearTerminal();
+        // while there is stuff in the frontier:
+        // get the next location
+        // process the location to find the locations (use the maze to do this)
+        // check if any locations are the end, if you found the end just return true!
+        // add all the locations to the frontier
+        while (frontier.hasNext()) {
+            if (animate) {
                 System.out.println(maze.toStringColor());
             }
-            
+
             Location location = frontier.next();
-            // Location[] neighbors = maze.getNeighbors(location);
-            if(location.getX() == maze.getEnd().getX() && location.getY() == maze.getEnd().getY()){
+            if (location.getX() == maze.getEnd().getX() && location.getY() == maze.getEnd().getY()) {
                 while (location != null) {
                     System.out.println(maze.toStringColor());
-                    maze.set(location.getX(),location.getY(),'@');
+                    maze.set(location.getX(), location.getY(), '@');
                     location = location.getPrevious();
                 }
-                // maze.set(maze.getStart().getX(),maze.getStart().getY(),'@');
                 return true;
             }
-            // maze.set(location.getX(),location.getY(), '.');
-            for(Location n : maze.getNeighbors(location)){
-                if(n != null){
-                    
+
+            maze.set(location.getX(), location.getY(), '.');
+
+            for (Location n : maze.getNeighbors(location)) {
+                if (n != null) {
+                    maze.set(n.getX(), n.getY(), '?');
                     frontier.add(n);
                 }
             }
         }
 
-      //when there are no more values in the frontier return false
-      return false;
+        // when there are no more values in the frontier return false
+        return false;
     }
 
-    public String toString(){
-      return maze.toString();
+    public String toString() {
+        return maze.toString();
     }
 
-   private void wait(int millis){
-        try {
-            Thread.sleep(millis);
-        }
-        catch (InterruptedException e) {
-        }
-     }
-
-    public void setAnimate(boolean b){
+    public void setAnimate(boolean b) {
         animate = b;
     }
 
-    public void clearTerminal(){
-        //erase terminal, go to top left of screen.
+    public void clearTerminal() {
+        // erase terminal, go to top left of screen.
 
         System.out.println("\033[2J\033[1;1H");
     }
 
     public static void main(String[] args) {
         MazeSolver n = new MazeSolver("maze.txt");
-        n.solve(0);
+        n.solve(1);
     }
 }
